e00d4b34746d4350e7b968085a6a0914802b4614
zuberkhan690 <zuberkhan69@gmail.com0>
2026-01-11 18:49:39 +0530

    comminting

---
+++ Task1.py
@@ -0,0 +1,15 @@
+""" Problem Statement: Write a Python program that does the following:
+"""
+# Takes two numbers as input from the user.
+
+Number1 = int(input("Enter the first number: "))
+Number2 = int(input("Enter the second number: "))
+
+# Performs the basic mathematical operations on these two numbers:
+addition = Number1 + Number2
+subtraction = Number1 - Number2
+multiplication = Number1 * Number2
+division = Number1 / Number2
+
+print("Value of addition is  ", addition ,"\nValue of substraction is  ",subtraction, "\nValue of multiplication is  ", multiplication , "\nValue of division is  ", division)
+
---
+++ Task2.py
@@ -0,0 +1,8 @@
+# Task 2: Create a Personalized Greeting
+# Takes a user's first name and last name as input.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#Concatenates the first name and last name into a full name
+print("First name entered is ",first_name, "\nlast name entered is ", last_name)
+print("Hello, ",first_name,last_name,"!", "welcome to python Programing ")
